e29fa5b7a24d3e36f58151d03137403653d1bb43
lydecast <lydecast@gmail.com>
2024-04-04 12:40:16 -0400

    started on making a card template

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -1,5 +1,17 @@
 <template>
-  <!-- your answer -->
+    <v-card>
+        <v-card-title>
+            {{item.name}}
+        </v-card-title>
+        <v-card-subtitle>
+            {{item.rating}} {{item.price}} {{item.stock}}
+            <<!--might implement using icons, though preferably not the kind the prof used and definitely avoiding changing the rating-->
+            <<!--also if this is too crowded, one or two things can be changed to a card action-->
+        </v-card-subtitle>
+        <v-card-text>
+        {{item.description}}
+        </v-card-text>
+    </v-card>
 </template>
 
 <script lang="ts" setup>
